53a97d738aa5059171f91fbe8f5bf83562f36379
shaileshminsnapsys <75966330+shaileshminsnapsys@users.noreply.github.com>
2023-10-10 16:21:02 +0530

    API URL uncomment

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const Rollbar = require('rollbar');
 const path = require('path');
 
-// exports.APIURL = 'https://api.fullmetal.ai/';
+exports.APIURL = 'https://api.fullmetal.ai/';
 
 require('dotenv').config({ path: path.resolve(__dirname, '.env') });
 exports.rollbar = new Rollbar({
